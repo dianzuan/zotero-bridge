cdb494be3eceefb5b576932fe6aa0bafc1a3d3cb
diamondrill <hongzuanlv@gmail.com>
2026-05-21 11:22:03 +0800

    Bump version to 0.2.4

--- plugin/scripts/setup-zotron.sh
+++ plugin/scripts/setup-zotron.sh
@@ -25,7 +25,7 @@ case ":${PATH}:" in
     ;;
 esac
 
-REQUIRED_VERSION="${ZOTRON_REQUIRED_VERSION:-0.2.3}"
+REQUIRED_VERSION="${ZOTRON_REQUIRED_VERSION:-0.2.4}"
 GITHUB_XPI_URL="https://github.com/dianzuan/zotron/releases/download/v${REQUIRED_VERSION}/zotron.xpi"
 DEFAULT_XPI_URLS="${GITHUB_XPI_URL}
 https://gh-proxy.com/${GITHUB_XPI_URL}
